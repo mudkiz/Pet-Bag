230c9e9e2fc797f4b193d07b62cc6fb64940657a
Austin Niwa <scarletdragon19950@gmail.com>
2023-03-28 23:21:52 -0400

    Add files via upload
    Cat file for IT-145

---
+++ cat.java
@@ -0,0 +1,20 @@
+package cat;
+
+public class Cat{ 
+    private int catSpace;
+    //constructor
+    public Cat() { 
+        catSpace = 0; 
+    }
+
+    //Set cat space number
+
+    public void setCatSpace (int space) { 
+        this.catSpace = space;
+    }
+
+    //Get cat space number
+
+    public int getCatSpace () { 
+        return catSpace;
+    }
